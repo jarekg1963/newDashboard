08c5649db74d66a5f770f5969da9420a499b23ab
jarekg1963 <jarekg@test.com.pl>
2019-11-22 15:25:17 +0100

    Wykresy ok

--- DashBoard/src/app/charts/pie-chart/pie-chart.component.ts
+++ DashBoard/src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { THEME_COLORS } from '../../shared/theme.colors';
 
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
   styleUrls: ['./pie-chart.component.css']
 })
+
+
 export class PieChartComponent implements OnInit {
+
+  theme = 'Bright';
+
+  @Input() inputData: any;
+  @Input() limit: number;
+
+  pieChartData: number[] = [350, 450 , 120];
+  pieChartLabels: string[] = ['XYX ', 'ZZZZ' , 'AAAAA']
+
+  colors: any[] = [
+    {
+      backgroundColor: this.themeColors(this.theme),
+      borderColor: '#111'
+    }
+  ];
+
+  pieChartType = 'doughnut';
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  themeColors(setName: string): string[] {
+    const c = THEME_COLORS.slice(0)
+      .find(set => set.name === setName).colorSet;
+
+    return c;
   }
 
 }
