163c01492cdbe476103bdbcf3a94f893098a787e
jarekg1963 <jarekg@test.com.pl>
2019-11-22 15:44:18 +0100

    Orders component

--- DashBoard/src/app/sections/section-orders/section-orders.component.ts
+++ DashBoard/src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/shared/order';
 
 @Component({
   selector: 'app-section-orders',
@@ -6,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section-orders.component.css']
 })
 export class SectionOrdersComponent implements OnInit {
+
+  orders: Order[] = [
+    {id: 1, customer:
+      {id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
+      total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 3)},
+    {id: 2, customer:
+      {id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
+      total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 3)},
+    {id: 3, customer:
+      {id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
+      total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 3)},
+    {id: 4, customer:
+      {id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
+      total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 3)},
+    {id: 5, customer:
+      {id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
+      total: 230, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 3)},
+  ];
+
+
+
+  total = 0;
+  page = 1;
+  limit = 10;
+  loading = false;
 
   constructor() { }
 
